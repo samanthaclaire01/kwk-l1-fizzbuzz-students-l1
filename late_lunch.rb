d39ca9b3be7d97afac70fcee4270932348fee97d
Samantha Cassity <cassityclaire@gmail.com>
2018-06-05 21:51:36 +0000

    Done.

---
+++ late_lunch.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(i)
+  if i % 3 == 0
+    puts "buzz"
+  elsif i % 5 == 0
+  puts "fizz"
+  elsif i % 5 ==0 && i % 3 == 0
+  puts "fizzbuzz"
+else
+  puts "doesn't work"
+
+end
+end
+fizzbuzz(4)
